54911f0a2b929a79a6ccc17998f20929f798ef45
tboonma <tboonma@hotmail.com>
2021-10-11 23:57:10 +0700

    Initialize Tailwind CSS

--- threpose/settings.py
+++ threpose/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'tailwind',
+    'theme'
+]
+
+# Config for Tailwind CSS
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 MIDDLEWARE = [

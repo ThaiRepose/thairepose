cecfcdc7d46f4fc8b16f90ff243004e04535792c
Nabhan Suwanachote <nabhan.au@gmail.com>
2021-10-18 18:48:49 +0700

    add test email

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.http import response
+from django.test import TestCase, Client, client
+from django.core import mail
+from .views import send_action_email
+from .models import Customer
 
 # Create your tests here.
+class TestEmailSend(TestCase):
+    """Test send email."""
+
+    def email_backend_setup(self, settings):
+        settings.EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+    def user_setup(self):
+        self.user = Customer.objects.create_user('Tester', 'test@example.com', 'Password123')
+
+    def test_send(self):
+        mail.send_mail('subject', 'body.', 'from@example.com', ['to@example.com'])
+        assert len(mail.outbox) == 1
+
+class TestRegister(TestCase):
+    """Test user register"""
+
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reversed('register')
+    
+    def email_backend_setup(self, settings):
+        settings.EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+
+    def test_template(self):
+        pass

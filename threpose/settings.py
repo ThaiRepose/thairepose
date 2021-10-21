b411b18ebe38e7f157666b6721c0ced39afe3b9a
Nabhan Suwanachote <nabhan.au@gmail.com>
2021-10-21 17:00:03 +0700

    remove path

--- threpose/settings.py
+++ threpose/settings.py
@@ -52,8 +52,6 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',

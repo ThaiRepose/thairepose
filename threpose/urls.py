8524146902f2f68c5d399aaa65ad1e768bc49e2f
tboonma <tboonma@hotmail.com>
2021-10-26 15:45:52 +0700

    Add navigation to trip app

--- threpose/urls.py
+++ threpose/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views
+from users import views as usersviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('users/', usersviews.index),
+    path('', include('trip.urls', namespace="trip"))
 ]

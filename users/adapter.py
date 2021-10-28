d841335de39af6f1066d59473b000b9c7a072358
Nabhan Suwanachote <nabhan.au@gmail.com>
2021-10-28 12:57:30 +0700

    adapter for social account signup for store data in profile model

--- users/adapter.py
+++ users/adapter.py
@@ -12,20 +12,16 @@ class ProfileAccountAdapter(DefaultAccountAdapter):
         Profile.objects.create(
             user = user
         )
-        print("save no Oauth")
         return user
 
 
-# class ProfileSocialAccountAdapter(DefaultSocialAccountAdapter):
-#     def save_user(self, request, sociallogin, form=None):
-#         user = super(ProfileSocialAccountAdapter, self).save_user(
-#             request, sociallogin, form
-#         )
-#         picture_url = sociallogin.account.get_avatar_url()
-#         print(picture_url)
-#         Profile.objects.create(
-#             user = user,
-#         )
-#         print("save social user")
-
-#         return user
+class ProfileSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def save_user(self, request, sociallogin, form=None):
+        user = super(ProfileSocialAccountAdapter, self).save_user(
+            request, sociallogin, form
+        )
+        picture_url = sociallogin.account.get_avatar_url()
+        Profile.objects.create(
+            user = user,
+        )
+        return user

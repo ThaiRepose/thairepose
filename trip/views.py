53a423d562ccbfc01c884428e0d1fa7fe854e473
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-10-31 15:42:52 +0700

    fix pep8

--- trip/views.py
+++ trip/views.py
@@ -41,10 +41,12 @@ class AddReview(CreateView):
     model = Review
     template_name = "trip/add_review.html"
     fields = ('name', 'body')
-    
+
     def form_valid(self, form):
+        """Auto choose current post for add comment."""
         form.instance.post_id = self.kwargs['pk']
         return super().form_valid(form)
+
 
 def LikeView(request, pk):
     """Function to user like of each commend."""

7c8fcdf3c6317eb12acac47f4e9d7b3e549d7d87
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-11-05 19:19:29 +0700

    fix error when call model before migrate it.

--- trip/forms.py
+++ trip/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from .models import TripPlan, CategoryPlan
 
-choice = CategoryPlan.objects.all().values_list('name', 'name')
-choice_list = []
-for item in choice:
-    if item not in choice_list:
-        choice_list.append(item)
 
+# choice = CategoryPlan.objects.all().values_list('name', 'name')
+choice_list = ['Uncategorize']
+# for item in choice:
+#     if item not in choice_list:
+#         choice_list.append(item)
 
 class TripPlanForm(forms.ModelForm):
     """Class for create trip form."""
-
+    
     class Meta:
         model = TripPlan
         fields = ('title', 'author', 'duration', 'price', 'category', 'body')
@@ -18,3 +18,4 @@ class TripPlanForm(forms.ModelForm):
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'author': forms.TextInput(attrs={'class': 'form-control',  'value':'test', 'id':'test',}),
         }
+    

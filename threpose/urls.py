eb75ef55c539d4208b02023505e1727f27897266
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-11-12 16:18:20 +0700

    add media root and add to url and create folder pic instal trip static

--- threpose/urls.py
+++ threpose/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('trip.urls', namespace="trip")),
-]
+    path('ckeditor/', include('ckeditor_uploader.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

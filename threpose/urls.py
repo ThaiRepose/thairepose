97e19b7b66839023e56a6b6ff3b4da0e83617e2c
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-11-12 14:41:09 +0700

    add css

--- threpose/urls.py
+++ threpose/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('trip.urls', namespace="trip")),
-    path('profile/', views.profile, name='profile'),
+    path('profile', views.profile, name='profile'),
     path('editprofile/', views.edit_profile, name='editprofile')
 ]
 

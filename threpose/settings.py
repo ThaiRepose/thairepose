6b2b91c282879df6160a3447317d6b1a6e1c3653
vitvara <gridxlgm@gmail.com>
2021-11-21 20:25:04 +0700

    remove npm path from setting.py

--- threpose/settings.py
+++ threpose/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 

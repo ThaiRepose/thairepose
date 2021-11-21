f13826b29dbc7efd3a19bcb4782030b09f7dc369
tboonma <tboonma@hotmail.com>
2021-11-21 10:17:45 +0700

    Create tests for Models

--- planner/tests/test_models.py
+++ planner/tests/test_models.py
@@ -1,14 +1,109 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.test import TestCase
-from .models import Place, Plan, Editor
+
+from ..models import Place, Plan, Editor
 
 
-class PlanModelTest(TestCase):
-    """Tests for plan model implementing."""
+class PlannerModelTest(TestCase):
+    """Initialized tests for all method in Planner."""
 
     def setUp(self):
-        """Initialize."""
-        pass
+        """Initialize user and a plan."""
+        self.username = "ThaiRepose"
+        self.password = "TawanBoonma"
+        self.email = "thairepose@mail.com"
+        self.user = User.objects.create_user(username=self.username,
+                                             email=self.email,
+                                             password=self.password)
+        self.plan = Plan.objects.create(name="Test", days=2, author=self.user)
 
-    def test_create_plan(self):
-        """Test creating 1 planner."""
-        self.plan = Plan.objects.create
+
+class PlanModelTest(PlannerModelTest):
+    """Tests for plan model using."""
+
+    def setUp(self):
+        """Initialize user and a plan."""
+        super().setUp()
+
+    def test_model_name(self):
+        """Test that object can display name correctly."""
+        self.assertEqual(self.plan.__str__(), "Test")
+
+    def test_is_editable(self):
+        """Test is_editable() method in Plan."""
+        other_user = User.objects.create_user(username="Guest",
+                                              email="guest@mail.com",
+                                              password="NewGuest2021")
+        self.assertTrue(self.plan.is_editable(self.user))
+        self.assertFalse(self.plan.is_editable(other_user))
+        self.assertFalse(self.plan.is_editable(None))
+        self.assertFalse(self.plan.is_editable(True))
+        Editor.objects.create(plan=self.plan, user=other_user, role=1)
+        self.assertTrue(self.plan.is_editable(other_user))
+
+    def test_is_viewable(self):
+        """Test is_viewable() method in Plan."""
+        other_user = User.objects.create_user(username="Guest",
+                                              email="guest@mail.com",
+                                              password="NewGuest2021")
+        self.assertTrue(self.plan.is_viewable(self.user))
+        self.assertFalse(self.plan.is_viewable(other_user))
+        self.assertFalse(self.plan.is_viewable(None))
+        self.assertFalse(self.plan.is_viewable(True))
+        Editor.objects.create(plan=self.plan, user=other_user, role=1)
+        self.assertTrue(self.plan.is_viewable(other_user))  # Editor user can view plan too.
+        self.plan.status = 1
+        self.assertTrue(self.plan.is_viewable(None))  # Now everyone should be able to view this plan.
+
+
+class EditorModelTest(PlannerModelTest):
+    """Test for Editor model using."""
+
+    def setUp(self):
+        """Initialize user and a plan."""
+        super().setUp()
+        self.other_user = User.objects.create_user(username="Guest",
+                                                   email="guest@mail.com",
+                                                   password="NewGuest2021")
+        self.editor = Editor.objects.create(user=self.other_user, plan=self.plan, role=1)
+
+    def test_name_spelling(self):
+        """Test that object __str__() output correctly."""
+        self.assertEqual(self.editor.__str__(), f"{self.plan} - {self.other_user}")
+
+
+class PlaceModelTest(PlannerModelTest):
+    """Test for Place model using."""
+
+    def setUp(self):
+        """Initialize user and a plan."""
+        super().setUp()
+        self.first_place = Place.objects.create(day=1,
+                                                sequence=1,
+                                                place_id="PLACE_ID",
+                                                place_name="SIAM",
+                                                place_vicinity="Bangkok",
+                                                arrival_time=None,
+                                                departure_time=datetime.time(0, 0, 0),
+                                                plan=self.plan)
+        self.second_place = Place.objects.create(day=1,
+                                                 sequence=2,
+                                                 place_id="PLACE_ID2",
+                                                 place_name="SIAM2",
+                                                 place_vicinity="Bangkok2",
+                                                 arrival_time=None,
+                                                 departure_time=datetime.time(0, 0, 0),
+                                                 plan=self.plan)
+
+    def test_locate_place(self):
+        """Check order of places in the plan."""
+        self.assertIn(self.first_place, self.plan.place_set.all())
+        self.assertEqual(self.first_place.sequence, 1)  # sequence starts at 1
+        self.assertEqual(self.first_place, self.plan.place_set.all()[0])
+        self.assertEqual(self.second_place, self.plan.place_set.all()[1])
+
+    def test_place_spelling(self):
+        """Test that object can display place object correctly in string format."""
+        self.assertEqual(self.first_place.__str__(), "SIAM")

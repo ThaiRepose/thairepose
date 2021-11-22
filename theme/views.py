a1197b5dc8ebfc4fcaaa063517d01b541e3d1849
tboonma <tboonma@hotmail.com>
2021-11-22 19:26:54 +0700

    Add some additional pages

--- theme/views.py
+++ theme/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def about_us(request):
     """Render About us page."""
     return render(request, "about-us.html")
+
+
+def feedback(request):
+    """Render collect feedback page."""
+    return render(request, "feedback.html")

23f32f22b463ac05e05a1b87406c2dbbf6892d6e
tboonma <tboonma@hotmail.com>
2021-11-22 21:50:40 +0700

    Initialize E2E testing

---
+++ test_e2e/test_e2e_planner.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.urls import reverse
+
+
+def login(browser: webdriver, url: str, username: str, password: str) -> webdriver:
+    """Login with specified username and password.
+
+    Args:
+        browser: browser object that is initialized.
+        url: login url.
+        username: user's username.
+        password: user's password.
+
+    Returns:
+        browser object that is logged in.
+    """
+    browser.get(url)
+    username_box = browser.find_element_by_xpath('//*[@id="id_login"]')
+    username_box.send_keys(username)
+    password_box = browser.find_element_by_xpath('//*[@id="id_password"]')
+    password_box.send_keys(password)
+    # click login button
+    browser.find_element_by_xpath('/html/body/div/div[1]/div/div/div/div[2]/form/div[3]/button').click()
+    return browser
+
+
+class E2ETestPlanner(StaticLiveServerTestCase):
+    """Tests for Planner app."""
+
+    def setUp(self):
+        """Setting up browser."""
+        self.url = self.live_server_url
+        # create user and login
+        self.user_detail = {"username": "Tawan", "password": "ThaiRepose"}
+        self.user = User.objects.create_user(username=self.user_detail['username'], password=self.user_detail['password'])
+        self.user.save()
+
+        # Initialize webdriver
+        options = Options()
+        # options.add_argument("--headless")
+        self.browser = webdriver.Firefox(
+            executable_path="/Users/tawaneiei/Desktop/KU/ISP/selenium-exercise/geckodriver", options=options)
+        self.browser = login(self.browser, self.url+reverse("account_login"),
+                             self.user_detail['username'], self.user_detail['password'])
+        self.browser.implicitly_wait(1)  # seconds
+
+    def tearDown(self):
+        input("Press any key to exit.")
+        self.browser.quit()
+        super(E2ETestPlanner, self).tearDown()
+
+    def test_planner_creation(self):
+        """Test that the planner can be created."""
+        self.browser.get(self.url + reverse("planner:index"))
+        # create new planner button
+        self.browser.find_element_by_xpath('/html/body/div/div[1]/div/div[2]/div/a').click()
+        self.browser.implicitly_wait(2)
+
+    def test_increase_day(self):
+        """Test increasing day in trip planner."""
+        pass

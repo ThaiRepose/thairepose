f7d082b1ec80692bde7d7968db2fb903260caef3
vitvara <gridxlgm@gmail.com>
2021-11-22 15:57:32 +0700

    fix flake8

--- users/views.py
+++ users/views.py
@@ -40,9 +40,11 @@ def profile(request):
                }
     return render(request, "users/profile.html", context)
 
+
 def privacy_policy(request):
     return render(request, "policy/privacy_policy.html")
-    
+
+
 @login_required
 def edit_profile(request):
     """Config fiel of user form and profile form.

245aeef3043e1161d64910144bbe2350672beb6d
tboonma <tboonma@hotmail.com>
2021-11-27 00:33:56 +0700

    Fix code quality by Flake8

--- threpose/settings.py
+++ threpose/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='its-no-secret')
-DEFAULT_FROM_EMAIL='no-reply@thairepose.com'
-SERVER_MAIL=DEFAULT_FROM_EMAIL
-ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
+DEFAULT_FROM_EMAIL = 'no-reply@thairepose.com'
+SERVER_MAIL = DEFAULT_FROM_EMAIL
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 

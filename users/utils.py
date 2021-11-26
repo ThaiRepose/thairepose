e4e95491746041bb05f3f13ccbf7e96bcfe5cbb3
vitvara <gridxlgm@gmail.com>
2021-11-26 15:37:33 +0700

    fix bug profile page

--- users/utils.py
+++ users/utils.py
@@ -23,7 +23,7 @@ def upload_profile_pic(user, image_url, filename, testing=False):
             File(open(result[0], 'rb'))
         )
     except:
-        result = os.path.join(os.path.join('user', 'profile_picture'),
+        result = os.path.join(os.path.join('media', 'user', 'profile_picture'),
                               "blank-profile-picture.png")
         user.profile.profile_pic.save(
             filename,
